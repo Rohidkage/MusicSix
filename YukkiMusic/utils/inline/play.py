87331fe36073f2e95917368bc74070706815dbae
KAGE <110266102+Rohidkage@users.noreply.github.com>
2023-05-01 01:47:58 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -17,7 +17,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 text=_["PL_B_3"],  switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/Nyeghhh",
+                text=_["S_B_9"], url=f"https://t.me/Kagbunshiiin",
             ),
         ],
         [
@@ -67,7 +67,7 @@ def stream_markup(_, videoid):
                 text=_["PL_B_3"], switch_inline_query_current_chat=""
             ),
             InlineKeyboardButton(
-                text=_["S_B_9"], url=f"https://t.me/gbtnyaOtan",
+                text=_["S_B_9"], url=f"https://t.me/kagestore69",
             ),
         ],
         [
