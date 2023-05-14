f1b5abc5810ec5d6931d2d4b4de46771795c29aa
KAGE <110266102+Rohidkage@users.noreply.github.com>
2023-05-15 03:17:57 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,7 +26,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-JOIN = getenv("JOIN", "gbtnyaOtan")
+JOIN = getenv("JOIN", "kagestore69")
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 DURATION_LIMIT_MIN = int(
